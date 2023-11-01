f55dc278db331743d95c00f070c0d6df31e28954
tcodes317 <tcodes317@gmail.com>
2023-11-01 13:02:58 -0700

    calculator

---
+++ script/app.js
@@ -0,0 +1,4 @@
+setTimeout(()=>{
+    location.reload();
+}, 3000);
+let input=document.getElementById("input") ;let one=document.getElementById("one");let two=document.getElementById("two");let three=document.getElementById("three");let four=document.getElementById("four");let five=document.getElementById("five");let six=document.getElementById("six");let seven=document.getElementById("seven");let eight=document.getElementById("eight");let nine=document.getElementById("nine");let ten=document.getElementById("ten");one.addEventListener("click", ()=>{let o=input.value="1";})
